ba00db344fe70c8674640fc3fed722ebb2672acf
sunpyopark <photo10004@gmail.com>
2020-11-01 12:37:59 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 0.11.0"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  availability_zone = "${var.aws_region}b"
+  tags = {
+    Name = "${var.name}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-west-1"
+}
+
+variable "ami_id" {
+  description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
+  default = "ami-021809d9177640a20"
+}
+
+variable "instance_type" {
+  description = "type of EC2 instance to provision."
+  default = "t2.micro"
+}
+
+variable "name" {
+  description = "name to pass to Name tag"
+  default = "Provisioned by Terraform"
+}
